5e26de9568d804c6532deb1d42922ed524997f90
Sothus <krzysztof.plucinski.95@gmail.com>
2018-03-10 17:46:40 +0100

    changed folder naming. Created hello world page

--- super_smartpihome/wsgi.py
+++ super_smartpihome/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for SmartPiHome project.
+WSGI config for super_smartpihome project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SmartPiHome.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "super_smartpihome.settings")
 
 application = get_wsgi_application()
