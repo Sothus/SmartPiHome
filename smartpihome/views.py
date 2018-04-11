7020246eba908edfeb58670eb321c65835ecb12a
Sothus <krzysztof.plucinski.95@gmail.com>
2018-04-11 12:24:13 +0200

    Added specify html templates and redirecting to them from navbar

--- smartpihome/views.py
+++ smartpihome/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
 def home_page(request):
-	return render(request, 'base.html')
+	return render(request, 'home.html')
+
+def light_page(request):
+	return render(request, 'light.html')
+
+def temperature_page(request):
+	return render(request, 'temperature.html')
+
+def sensor_page(request):
+	return render(request, 'sensor.html')
+
+def statistic_page(request):
+	return render(request, 'statistic.html')
+
+def about_page(request):
+	return render(request, 'about.html')
 
 # Create your views here.
--- super_smartpihome/urls.py
+++ super_smartpihome/urls.py
@@ -19,5 +19,10 @@ from smartpihome import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
+    url(r'^light/', views.light_page, name='light'),
+    url(r'^temperature/', views.temperature_page, name='temperature'),
+    url(r'^sensor/', views.sensor_page, name='sensor'),
+    url(r'^statistic/', views.statistic_page, name='statistic'),
+    url(r'^about/', views.about_page, name='about'),
     url(r'^admin/', admin.site.urls),
 ]
