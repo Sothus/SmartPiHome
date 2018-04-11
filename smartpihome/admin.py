74bdc44a157be64fe7dd15960905cae1bb317411
Sothus <krzysztof.plucinski.95@gmail.com>
2018-04-11 13:17:36 +0200

    Done migrations of the models to database. Created superuser(admin, adminadmin). Registered models in admin page

--- smartpihome/admin.py
+++ smartpihome/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import RaspberryPi, Light, RGBLight, DistanceSensor
+
+class RaspberryPiAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address']
+admin.site.register(RaspberryPi, RaspberryPiAdmin)
+
+class LightAdmin(admin.ModelAdmin):
+    list_display = ['name', 'pin']
+admin.site.register(Light, LightAdmin)
+
+class RGBLightAdmin(admin.ModelAdmin):
+    list_display = ['name', 'pin']
+admin.site.register(RGBLight, RGBLightAdmin)
+
+class DistanceSensorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'trigger_pin', 'echo_pin']
+admin.site.register(DistanceSensor, DistanceSensorAdmin)
+
 
 # Register your models here.
